8aad3818cbbc8df56b860d051307b6bd6999a43c
Tripti Ahuja <triptiahuja27@gmail.com>
2026-05-26 17:01:58 +0530

    chore

--- practice_python.py
+++ practice_python.py
@@ -1,11 +1,10 @@
-def smallest_odd_digit(num):
-    smallest = -1 
-    for i in str(num):
-        digit = int(i)
-        if digit % 2 != 0:
-            if smallest == -1 or digit < smallest:
-                smallest = digit
-    return smallest
+def largest_digit(num):
+    largest = 0
+    for digit in str(num):
+        digit = int(digit)
+        if digit > largest:
+            largest = digit
+    return largest
 
-print(smallest_odd_digit(57391))
+print(largest_digit(7))
     
