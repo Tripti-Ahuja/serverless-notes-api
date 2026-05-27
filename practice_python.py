f693d0bcb73688e92cd95c8f982cad6bd7081dd0
Tripti Ahuja <triptiahuja27@gmail.com>
2026-05-27 14:47:23 +0530

    second_largest_number

--- practice_python.py
+++ practice_python.py
@@ -1,10 +1,17 @@
-def largest_digit(num):
-    largest = 0
+def second_largest_number(num):
+    largest = -1
+    second_largest = -1
     for digit in str(num):
         digit = int(digit)
         if digit > largest:
+            second_largest = largest
             largest = digit
-    return largest
+        elif digit < largest and digit > second_largest:
+            second_largest = digit
+    return second_largest
 
-print(largest_digit(7))
+
+print(second_largest_number(19345))
+    
+
     
