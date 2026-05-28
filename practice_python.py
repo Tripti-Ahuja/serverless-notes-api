554f8cfa5a464024472cab01425ce069e6ad54eb
Tripti Ahuja <triptiahuja27@gmail.com>
2026-05-28 18:41:01 +0530

    categorize digits

--- practice_python.py
+++ practice_python.py
@@ -1,14 +1,17 @@
-def classify_number(n):
-    if n == 0:
-        return "Zero"
-    elif n < 0:
-        return "negative"
-    elif n > 0 and n % 2 == 0:
-        return "positive even"
-    elif n > 0 and n % 2 != 0:
-        return "positive odd"
-    
-print(classify_number(2))
-    
+def categorize_digits(num):
+    low = 0
+    mid = 0
+    high = 0
+    for digit in str(num):
+        digit = int(digit)
+        if digit <= 3: #>= 0 and digit <= 3:
+            low += 1 
+        elif digit <= 6:
+            mid += 1
+        else:
+            high += 1
+    return f"low: {low}, mid: {mid}, high : {high}"
+        
+print(categorize_digits(13579))
 
     
