1705dbec18e4587464ab81e50d275228c4cb11f6
Tripti Ahuja <triptiahuja27@gmail.com>
2026-06-09 22:59:52 +0530

    chore

---
+++ Basic_python.py
@@ -0,0 +1,28 @@
+"""name = 'Tripti'
+age = '29'
+had_coffee = 'No'
+print(f"Hi, I'm {name}, {age} years old and I had {had_coffee} coffee today")"""
+
+"""name = 'tripti'    #str
+age = '29'         #age
+had_coffee = 'Yes' #bool
+points = '0.25'    #float
+print(type(name))
+print(type(age))
+print(type(had_coffee))
+print(type(points))"""
+
+"""name = input("Enter your name: ")
+yob = int(input("Enter your yob: "))
+current_year = int(2026)
+current_age = current_year-yob
+print(f"{name}, you are {current_age} years old.")"""
+
+"""print(5 + 3)"""
+
+name = input()
+price = float(input())
+quantity = int(input())
+total = price * quantity
+print(total)
+print(f"{quantity}x {name} = ${total}")
